13076bf39ab571d10b80626c4576a9a66ab841cf
deepikarastogi <rastogideepika18@gmail.com>
2024-08-13 12:48:49 +0530

    to find max numbers

---
+++ src/ex_10082024/Lab002.py
@@ -0,0 +1,5 @@
+#program to find maximum number between two integers
+#34,100,150
+print(max(34,100,150))
+print(max(34,100,150,-1,-100,45.8,34.6))
+#print(max(34,100,150,-1,-100,45.8,34.6,"Deepika"))- this line will give error as cannot compare integer and string
